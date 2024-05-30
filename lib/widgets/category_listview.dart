eba63197c8331a273ef76ed6c852bd4a2dddebb6
shenoda <engshenoda2020@gmail.com>
2024-05-30 22:06:43 +0300

    add imges

--- lib/widgets/category_listview.dart
+++ lib/widgets/category_listview.dart
@@ -5,12 +5,13 @@ import 'package:news_app/widgets/category_list_view.dart';
 class CategoryListView extends StatelessWidget {
   const CategoryListView({super.key});
   final List<CategoryModel> category = const [
-    CategoryModel(imges: 'assets/natural.jpg', categoryname: 'General'),
     CategoryModel(imges: 'assets/sport.jpg', categoryname: 'sport'),
-    CategoryModel(imges: 'assets/technology.jpeg', categoryname: 'Business'),
+    CategoryModel(imges: 'assets/Business.jpg', categoryname: 'Business'),
     CategoryModel(
-        imges: 'assets/entertaiment.avif', categoryname: 'Entertainment'),
-    CategoryModel(imges: 'assets/health.avif', categoryname: 'Health'),
+        imges: 'assets/Entertainment.jpg', categoryname: 'Entertainment'),
+    CategoryModel(imges: 'assets/Health.jpg', categoryname: 'Health'),
+    CategoryModel(imges: 'assets/technology.jpg', categoryname: 'technology'),
+    CategoryModel(imges: 'assets/science.jpg', categoryname: 'science'),
   ];
   @override
   Widget build(BuildContext context) {
