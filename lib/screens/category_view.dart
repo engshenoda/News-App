b5c3cc17336f10e74b4daa31d04c937cf8093be1
shenoda <engshenoda2020@gmail.com>
2024-05-30 22:06:43 +0300

    add imges

--- lib/screens/category_view.dart
+++ lib/screens/category_view.dart
@@ -7,6 +7,24 @@ class CategoryVeiw extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        title: const Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              "News",
+              style: TextStyle(color: Colors.black),
+            ),
+            Text(
+              "Cloud",
+              style: TextStyle(color: Colors.orange),
+            ),
+          ],
+        ),
+        centerTitle: true,
+      ),
       body: SizedBox(
           child: CustomScrollView(
         physics: const BouncingScrollPhysics(),
