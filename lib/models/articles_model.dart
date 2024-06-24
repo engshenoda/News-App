7c2acfa01f34af9737bfc14837ab1ce35f94c6e3
shenoda <engshenoda2020@gmail.com>
2024-06-24 16:32:01 +0300

    add web veiw

--- lib/models/articles_model.dart
+++ lib/models/articles_model.dart
@@ -2,12 +2,14 @@ class ArticleModel {
   final String? image;
   final String title;
   final String? subtitle;
+  final String url;
 
-  ArticleModel(
-      {required this.image, required this.title, required this.subtitle});
+  ArticleModel( 
+      {required this.url,required this.image, required this.title, required this.subtitle});
 
   factory ArticleModel.fromJson(Map<String, dynamic> json) {
     return ArticleModel(
+      url : json['url'],
       image: json['urlToImage'],
       title: json['title'],
       subtitle: json['description'],
